a6da64210a1cec7dce183f359f82f3a54a7a8bb0
Lars Gunther <gunther@keryx.se>
2011-12-18 16:32:39 +0100

    prime test explained

--- is_prime.js
+++ is_prime.js
@@ -25,6 +25,16 @@ function isPrime(num) {
     return true;
 }
 
+// Explaining this version
+/*
+ * For 50 % of all numbers - the even ones - it will be O(1) 
+ * For most numbers it will break quickly, on 3, 5, 7, etc
+ * When reaching Math.sqrt(num) it will stop, since continuing would
+ * only mean doing redundant double checks
+ * Math.sqrt is calculated once BEFORE the loop O(1) and not in every iteration O(n)
+ *
+ * There are still some redundant checks (9, 15, 21, etc)
+ */
 
 //Shuld say "false and true"
 alert(String(isPrime(779)) + " and " + String(isPrime(787)));
