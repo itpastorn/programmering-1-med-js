abda8306ac649b4af0f0b4c66a8bee879732e15d
Lars Gunther <gunther@keryx.se>
2011-12-18 16:15:54 +0100

    prime test very ineffecient

--- is_prime.js
+++ is_prime.js
@@ -3,20 +3,21 @@
 
 // Move between checked in versions to see how the function evolves
 
-// Iteration 0 - insanely stupid
-// Tries to divide by every number
+// Iteration 1 - very inefficient
+// Tries to divide by every number, but halts at the square root
 function isPrime(num) {
     if (num !== Math.floor(num) || num < 1) {
         throw new Error("Argument num must be a positive integer");
     }
     var is_prime = true;
-    for ( var i = 2; i < num; i += 1 ) {
+    for ( var i = 2; i < Math.sqrt(num); i += 1) {
         if (num % i === 0) {
             is_prime = false;
         }
     }
     return is_prime;
 }
+
 
 //Shuld say "false and true"
 alert(String(isPrime(779)) + " and " + String(isPrime(787)));
