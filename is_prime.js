0f217a42428d3fa6499b6d9f873f9ae831929e59
Lars Gunther <gunther@keryx.se>
2011-12-18 16:17:52 +0100

    prime test barely usable

--- is_prime.js
+++ is_prime.js
@@ -3,22 +3,24 @@
 
 // Move between checked in versions to see how the function evolves
 
-// Iteration 2 - inefficient
-// Removed checks at 4, 6, 8, 10, etc
+
+// Iteration 3 - barely usable
+// Reduces redundant iterations
 function isPrime(num) {
     if (num !== Math.floor(num) || num < 1) {
         throw new Error("Argument num must be a positive integer");
     }
-    var is_prime = true;
     if (num % 2 === 0) {
-        is_prime = false;
+        return false;
     }
+    // Does not run if divisible by 2
     for ( var i = 3; i < Math.sqrt(num); i += 2) {
         if (num % i === 0) {
-            is_prime = false;
+            return false;
+            // implicit break
         }
     }
-    return is_prime;
+    return true;
 }
 
 
