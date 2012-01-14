3cc1d393454a651617157ef9ef6809ee0ca65d78
Lars Gunther <gunther@keryx.se>
2012-01-14 21:18:21 +0100

    A template

---
+++ template-1.js
@@ -0,0 +1,10 @@
+/* jshint forin:true, eqnull:true, noarg:true, noempty:true, eqeqeq:true, strict:true,
+   undef:true, curly:true, browser:true, devel:true, es5:true, indent:4, maxerr:50, white:true */
+
+"use strict"; // JSHint kommer klaga "Use the function form". Tills vidare är detta dock OK.
+
+/**
+ * Skriv kortfattat syfte med denna fil här
+ */
+
+// Börja koda här
